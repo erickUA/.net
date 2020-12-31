93ea371fbc4af25155d94affb3a971fc4da40234
Erick <erickurbina800@gmail.com>
2020-12-31 01:22:59 -0300

    mi primer commit oficial

---
+++ LeerData/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+
+namespace LeerData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+           using(var db = new AppVentaCursosContext()){
+
+               var cursos = db.Curso.Include(p => p.PrecioPromocion).AsNoTracking();
+               foreach (var curso in cursos)
+               {
+                   Console.WriteLine(curso.Titulo +"--------"+ curso.PrecioPromocion.PrecioActual);
+               }
+
+           }
+
+
+        }
+    }
+}
